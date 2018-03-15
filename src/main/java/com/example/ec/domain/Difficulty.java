120f28a7b0a79d489d81498f89fb12628a3bc9b7
Leandro Santos <leandro.ricardo.cba@gmail.com>
2018-03-15 10:50:24 -0400

    Created Services and Repository

---
+++ src/main/java/com/example/ec/domain/Difficulty.java
@@ -0,0 +1,14 @@
+package com.example.ec.domain;
+
+/**
+ * Created by maryellenbowman on 7/3/16.
+ */
+
+/**
+ * Enumeration of the level of effort.
+ * <p>
+ * Created by Mary Ellen Bowman
+ */
+public enum Difficulty {
+    Easy, Medium, Difficult, Varies;
+}
---
+++ src/main/java/com/example/ec/services/TourService.java
@@ -0,0 +1,45 @@
+package com.example.ec.services;
+
+import com.example.ec.domain.Difficulty;
+import com.example.ec.domain.Region;
+import com.example.ec.domain.Tour;
+import com.example.ec.domain.TourPackage;
+import com.example.ec.repo.TourPackageRepository;
+import com.example.ec.repo.TourRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TourService {
+
+    private TourRepository tourRepository;
+    private TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourService(TourRepository tourRepository, TourPackageRepository tourPackageRepository) {
+        this.tourRepository = tourRepository;
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    public Tour createTour(String title, String description, String blurb, Integer price, String duration, String bullets,
+                           String keywords, String tourPackageName, Difficulty difficulty, Region region) {
+        Optional<TourPackage> tourPackage = tourPackageRepository.findByName(tourPackageName);
+
+        if (!tourPackage.isPresent()) {
+            throw new RuntimeException("Tour package does not exist: " + tourPackageName);
+        } else {
+            return tourRepository.save(new Tour(title, description, blurb, price, duration, bullets,
+                    keywords, tourPackage.get(), difficulty, region));
+        }
+    }
+
+    public Iterable<Tour> lookup(){
+        return tourRepository.findAll();
+    }
+
+    public long total(){
+        return tourRepository.count();
+    }
+}
